d56a7c9024149e319c427fe6f556b2275a52c807
Erwin Qi <i@erw1n.dev>
2020-03-19 17:49:44 +0800

    Update tjvpn.sh
    remove sensitive info

--- tjvpn.sh
+++ tjvpn.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # 利用cookie连接同济VPN
 
-cookie=$(python /home/erwin/Telegram/TJVPN/tjvpnCookie.py 2>&1)
+cookie=$(python tjvpnCookie.py 2>&1)
 echo "获得Cookie: $cookie"
 sudo openconnect -q -b --juniper --no-dtls --cookie "DSID=${cookie}" vpn.tongji.cn
 echo "已连接"
